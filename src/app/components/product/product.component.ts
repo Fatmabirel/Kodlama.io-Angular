8b7839dfb59921332e6c2a006192de07e647e49c
Fatma Birel <Fatma Birel@DESKTOP-Q270QVE>
2024-05-15 15:45:14 +0300

    filterPipe

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/product';
 import { ProductService } from '../../services/product.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -10,17 +11,37 @@ import { ProductService } from '../../services/product.service';
 export class ProductComponent implements OnInit {
   products: Product[] = [];
   dataLoaded: boolean = false;
+  filterText:string = "";
 
-  constructor(private productService:ProductService) {}
+  constructor(
+    private productService: ProductService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.getProducts();
+    this.activatedRoute.params.subscribe(params=>{
+      if(params["categoryId"]){
+        this.getProductsByCategory(params["categoryId"])
+      }else{
+        this.getProducts();
+
+        
+      }
+    })
+    
   }
 
   getProducts() {
-    this.productService.getProducts().subscribe(response => {
+    this.productService.getProducts().subscribe((response) => {
       this.products = response.data;
       this.dataLoaded = true;
-    })
+    });
+  }
+
+  getProductsByCategory(categoryId:number) {
+    this.productService.getProductsByCategory(categoryId).subscribe((response) => {
+      this.products = response.data;
+      this.dataLoaded = true;
+    });
   }
 }
